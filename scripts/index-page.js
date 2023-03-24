6ba88f5a2524de042d6e94d8e12ace57b6741fca
Alfredo Armas <aarma028@gmail.com>
2023-03-24 15:39:35 -0400

    timestamps have been corrected

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -18,6 +18,13 @@ const comments = [
       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
   },
 ];
+var date = new Date();
+var current_date =
+  date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+var current_time =
+  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+var date_time = current_date + " " + current_time;
+
 //doesnt refresh page and adds the comment to the array!
 let form = document.querySelector(".form-submission");
 form.addEventListener("submit", (e) => {
@@ -26,9 +33,61 @@ form.addEventListener("submit", (e) => {
   console.log(e.target.comment.value);
   const newComment = {
     name: e.target.fullName.value,
-    timestamp: "03/23/2023",
+    timestamp: current_date,
     commentText: e.target.comment.value,
   };
   comments.unshift(newComment);
   console.log(comments);
+  createComments(comments);
+  e.target.reset();
 });
+
+const createComments = function (array) {
+  const commentMain = document.querySelector(".commentArray");
+  //   commentMain.innerHTML = "";
+  const commentHeader = document.createElement("div");
+  commentHeader.classList.add("commentHeader");
+  commentMain.appendChild(commentHeader);
+  const commentHeaderText = document.createElement("h1");
+  commentHeaderText.classList.add("mainHeaderText");
+  commentHeaderText.innerText = "Comments";
+  commentHeader.appendChild(commentHeaderText);
+  for (let i = 0; i < array.length; i++) {
+    const comment = array[i];
+    const commentContainer = document.createElement("div");
+    commentContainer.classList.add("commentContainer");
+    commentMain.appendChild(commentContainer);
+    // creating the comment image section
+    const commentImage = document.createElement("div");
+    commentImage.classList.add("commentImage");
+    commentContainer.appendChild(commentImage);
+    // creating the comment name section
+    const commentNameBox = document.createElement("div");
+    commentNameBox.classList.add("commentName");
+    commentContainer.appendChild(commentNameBox);
+    //adding the semantic specific tags to the div
+    const commentName = document.createElement("h5");
+    commentName.classList.add("commentName");
+    commentName.innerText = comment.name;
+    commentNameBox.appendChild(commentName);
+    // creating the timestamp section
+    const commentTimestampBox = document.createElement("div");
+    commentTimestampBox.classList.add("commentTimestampBox");
+    commentContainer.appendChild(commentTimestampBox);
+    //adding the semantic tags to the div
+    const commentTimestamp = document.createElement("p");
+    commentTimestamp.classList.add("commentTimestamp");
+    commentTimestamp.innerText = comment.timestamp;
+    commentTimestampBox.appendChild(commentTimestamp);
+    // creating the comment text section
+    const commentTextBox = document.createElement("div");
+    commentTextBox.classList.add("commentTextBox");
+    commentContainer.appendChild(commentTextBox);
+    //adding the semantic tags to the div
+    const commentText = document.createElement("p");
+    commentText.classList.add("commentText");
+    commentText.innerText = comment.commentText;
+    commentTextBox.appendChild(commentText);
+  }
+};
+createComments(comments);
